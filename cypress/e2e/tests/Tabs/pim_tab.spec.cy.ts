86420c0369c0bc6f5b32590f787f3787e61332b2
Mohammad Abohasan <mo7ammad.saed@gmail.com>
2023-09-23 01:01:46 +0300

    Wait Spinner

--- cypress/e2e/tests/Tabs/pim_tab.spec.cy.ts
+++ cypress/e2e/tests/Tabs/pim_tab.spec.cy.ts
@@ -15,30 +15,32 @@ describe('Login Page', () => {
     pimTab.openPIMTab();
   });
 
-  // it('Add employee', () => {
-  //   cy.get('@empInfo').then((infoData: any) => {
-  //     pimTab.addNewEmployee(infoData.firstName, infoData.middleName, infoData.lastName, infoData.id, infoData.userName, infoData.password, infoData.password);
+  it('Add employee', () => {
+    cy.get('@empInfo').then((infoData: any) => {
+      pimTab.addNewEmployee(infoData.firstName, infoData.middleName, infoData.lastName, infoData.id, infoData.userName, infoData.password, infoData.password);
 
-      // pimTab.editPersonalDetails(infoData.nickName, infoData.driversLicenseNumber, infoData.licenseExpiryDate, infoData.maritalStatus, infoData.dateOfBirth, infoData.gender);
-  //   });
-  // });
-
-  it('Search by key-value', () => {
-    pimTab.searchEmployee([
-      { key: "empName", value: "Aaliyah  Haq" }, 
-      { key: "empId", value: "0038" },
-      { key: "empStatus", value: "Full-Time Permanent" },
-      { key: "empInclude", value: "Current and Past Employees" },
-      { key: "empSupervisorName", value: "Odis Adalwin" },
-      { key: "empJobTitle", value: "QA Lead" },
-      { key: "empSubUnit", value: "Quality Assurance" },
-    ]);
+      pimTab.editPersonalDetails(infoData.nickName, infoData.driversLicenseNumber, infoData.licenseExpiryDate, infoData.maritalStatus, infoData.dateOfBirth, infoData.gender);
+    });
   });
 
+  // ===================================
+  // it('Search by key-value', () => {
+  //   pimTab.searchEmployee([
+  //     { key: "empName", value: "Aaliyah  Haq" }, 
+  //     { key: "empId", value: "0038" },
+  //     { key: "empStatus", value: "Full-Time Permanent" },
+  //     { key: "empInclude", value: "Current and Past Employees" },
+  //     { key: "empSupervisorName", value: "Odis Adalwin" },
+  //     { key: "empJobTitle", value: "QA Lead" },
+  //     { key: "empSubUnit", value: "Quality Assurance" },
+  //   ]);
+  // });
+  // ===================================
+
   afterEach(() => {
-  //   cy.get('@empInfo').then((infoData: any) => {
-  //     pimTab.deleteEmployee(infoData.id);
-  //   })
+    cy.get('@empInfo').then((infoData: any) => {
+      pimTab.deleteEmployee(infoData.id);
+    })
   });
 
 });
