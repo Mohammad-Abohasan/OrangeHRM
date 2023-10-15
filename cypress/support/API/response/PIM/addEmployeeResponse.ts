a747e15f0ccaed20f86d872e815b7b49cbb0262e
Mohammad-Abohasan <mo7ammad.saed@gmail.com>
2023-10-16 02:08:42 +0300

    add employee payload and response

---
+++ cypress/support/API/response/PIM/addEmployeeResponse.ts
@@ -0,0 +1,12 @@
+export interface ICreateEmployeeResponse {
+  data: {
+    employeeId: string;
+    empNumber: number;
+    firstName: string;
+    middleName: string;
+    lastName: string;
+    terminationId: number;
+  };
+  meta: [];
+  rels: [];
+}
