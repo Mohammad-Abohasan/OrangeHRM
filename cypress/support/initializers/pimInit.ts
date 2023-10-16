0c6c81f6198b005b8fd24092e4bf900f2004f309
Mohammad-Abohasan <mo7ammad.saed@gmail.com>
2023-10-16 04:27:28 +0300

    update PIM initializer

--- cypress/support/initializers/pimInit.ts
+++ cypress/support/initializers/pimInit.ts
@@ -1,16 +1,25 @@
 import { ICreateEmployeePayload } from "../API/payload/PIM/addEmployeePayload";
+import { IDeleteEmployeePayload } from "../API/payload/PIM/deleteEmployeePayload";
 
 export default class PIM {
   static initEmployee(
     employeeData: ICreateEmployeePayload
   ): ICreateEmployeePayload {
-    const {employeeId, firstName, middleName, lastName, empPicture} = employeeData;
+    const { employeeId, firstName, middleName, lastName, empPicture } =
+      employeeData;
     const payload = {
       employeeId,
       firstName,
       middleName,
       lastName,
       empPicture,
+    };
+    return payload;
+  }
+
+  static initDeleteEmployee(empNumber: number): IDeleteEmployeePayload {
+    const payload = {
+      ids: [empNumber],
     };
     return payload;
   }
