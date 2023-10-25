ee1ac46973a6f4189480de3107d2edd4cfd82b47
Mohammad Abohasan <mo7ammad.saed@gmail.com>
2023-10-25 03:10:11 +0300

    add new TC (Download xlsx file and verify)

--- cypress/e2e/tests/Tabs/recruitment_tab.spec.cy.ts
+++ cypress/e2e/tests/Tabs/recruitment_tab.spec.cy.ts
@@ -6,12 +6,13 @@ import candidatesHelper from "../../../support/helpers/candidatesHelper";
 import pimHelper from "../../../support/helpers/pimHelper";
 import commonHelper from "../../../support/helpers/commonHelper";
 
+import * as path from "path";
+
 const loginPage: LoginPage = new LoginPage();
 const candidatesPage: CandidatesPage = new CandidatesPage();
 const vacanciesPage: VacanciesPage = new VacanciesPage();
 
 let employeeData: any = {};
-
 describe("Recruitment: Candidates & Vacancies table data validation", () => {
   beforeEach(() => {
     cy.visit("/");
@@ -23,6 +24,111 @@ describe("Recruitment: Candidates & Vacancies table data validation", () => {
     cy.fixture("employeeInfo").then((empData) => {
       employeeData = empData;
     });
+  });
+
+  it("Recruitment - Vacancies: The user should be able to attach a file to a vacancy.", () => {
+    pimHelper
+      // Add an employee
+      .addEmployee(employeeData)
+      // Add a vacancy
+      .then((employeeResponse) => {
+        return cy.fixture("vacancyInfo").then((vacancyData) => {
+          return vacanciesHelper.addVacancy(
+            vacancyData,
+            employeeResponse.data.empNumber
+          );
+        });
+      })
+      // Attach a file to the vacancy
+      .then((vacancyResponse) => {
+        cy.visit(
+          `/web/index.php/recruitment/addJobVacancy/${vacancyResponse.data.id}`
+        );
+        vacanciesPage.addAttachment("cypress/fixtures/VacancyAttachment.xlsx");
+        cy.get(".oxd-toast").should("exist");
+        cy.fixture("vacancyAttachmentsInfo").then((attachmentsData) => {
+          commonHelper.checkRows(".oxd-table-row", attachmentsData);
+        });
+      })
+      // Delete the employee after the test
+      .then(() => {
+        return pimHelper.getEmployee(employeeData.employeeId);
+      })
+      .then((employeeResponse) => {
+        pimHelper.deleteEmployee(employeeResponse.data[0].empNumber);
+      });
+  });
+
+  it("Recruitment - Vacancies: The user should be able to download the vacancy attachment (excel file).", () => {
+    pimHelper
+      // Add an employee
+      .addEmployee(employeeData)
+      // Add a vacancy
+      .then((employeeResponse) => {
+        return cy.fixture("vacancyInfo").then((vacancyData) => {
+          return vacanciesHelper.addVacancy(
+            vacancyData,
+            employeeResponse.data.empNumber
+          );
+        });
+      })
+      // Attach a file to the vacancy
+      .then((vacancyResponse) => {
+        cy.visit(
+          `/web/index.php/recruitment/addJobVacancy/${vacancyResponse.data.id}`
+        );
+        const xlsxPath: string = "cypress/fixtures/VacancyAttachment.xlsx";
+        vacanciesPage.addAttachment(xlsxPath);
+        cy.get(".oxd-toast").should("exist");
+        cy.fixture("vacancyAttachmentsInfo").then((attachmentsData) => {
+          commonHelper.checkRows(".oxd-table-row", attachmentsData);
+        });
+        return cy
+          .task("convertXlsxToJson", [xlsxPath, true])
+          .then((jsonPathResponse: any) => {
+            return cy
+              .fixture(path.basename(jsonPathResponse))
+              .then((attachmentData) => {
+                // Delete the json file after the test
+                return cy.task("deleteFile", jsonPathResponse).then(() => {
+                  return [vacancyResponse.data.id, attachmentData];
+                });
+              });
+          });
+      })
+      // Download the attachment and verify the content
+      .then((vacancyResponse2) => {
+        const [vacancyResponseId, originalAttachmentData] = vacancyResponse2;
+        cy.visit(
+          `/web/index.php/recruitment/addJobVacancy/${vacancyResponseId}`
+        );
+        vacanciesPage.downloadAttachment();
+
+        const xlsxPath: string = "cypress/downloads/VacancyAttachment.xlsx";
+        cy.readFile(xlsxPath).should("exist"); // Verify that the file exists
+        let jsonPath: string = "";
+        cy.task("convertXlsxToJson", [xlsxPath, false])
+          .then((jsonPathResponse: any) => {
+            jsonPath = jsonPathResponse;
+            cy.fixture(path.basename(jsonPathResponse)).then(
+              (attachmentData) => {
+                expect(attachmentData).to.deep.equal(originalAttachmentData);
+              }
+            );
+          })
+          // Delete the downloaded files
+          .then(() => {
+            cy.task("deleteFile", xlsxPath);
+            cy.task("deleteFile", jsonPath);
+          });
+      })
+      // Delete the employee after the test
+      .then(() => {
+        return pimHelper.getEmployee(employeeData.employeeId);
+      })
+      .then((employeeResponse) => {
+        pimHelper.deleteEmployee(employeeResponse.data[0].empNumber);
+      });
   });
 
   it("Recruitment - Candidates: Attach Resume File", () => {
@@ -53,39 +159,6 @@ describe("Recruitment: Candidates & Vacancies table data validation", () => {
           `/web/index.php/recruitment/addCandidate/${candidateResponse.data.id}`
         );
         candidatesPage.addResume("cypress/fixtures/CandidateResume.docx");
-      })
-      // Delete the employee after the test
-      .then(() => {
-        return pimHelper.getEmployee(employeeData.employeeId);
-      })
-      .then((employeeResponse) => {
-        pimHelper.deleteEmployee(employeeResponse.data[0].empNumber);
-      });
-  });
-
-  it("Recruitment - Vacancies: The user should be able to attach a file to a vacancy.", () => {
-    pimHelper
-      // Add an employee
-      .addEmployee(employeeData)
-      // Add a vacancy
-      .then((employeeResponse) => {
-        return cy.fixture("vacancyInfo").then((vacancyData) => {
-          return vacanciesHelper.addVacancy(
-            vacancyData,
-            employeeResponse.data.empNumber
-          );
-        });
-      })
-      // Add a candidate
-      .then((vacancyResponse) => {
-        cy.visit(
-          `/web/index.php/recruitment/addJobVacancy/${vacancyResponse.data.id}`
-        );
-        vacanciesPage.addAttachment("cypress/fixtures/VacancyAttachment.xlsx");
-        cy.get(".oxd-toast").should("exist");
-        cy.fixture("vacancyAttachmentsInfo").then((attachmentsData) => {
-          commonHelper.checkRows(".oxd-table-row", attachmentsData);
-        });
       })
       // Delete the employee after the test
       .then(() => {
