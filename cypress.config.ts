eb68ed19ed8c8eca1908b3172f9cdb8d26fa01c8
Mohammad Abohasan <mo7ammad.saed@gmail.com>
2023-10-25 03:08:24 +0300

    `convertXlsxToJson` & `deleteFile` tasks

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,4 @@
-import { writeFileSync } from "fs";
+import { writeFileSync, unlink } from "fs";
 import { defineConfig } from "cypress";
 import allureWriter from "@shelex/cypress-allure-plugin/writer";
 import * as XLSX from "xlsx";
@@ -13,18 +13,30 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
       on("task", {
-        convertXlsxToJson(args : [string, boolean]) {
-          const [ filePath, isOriginalFile ] = args;
-          const workbook = XLSX.readFile(filePath);
-          const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-          const jsonData = XLSX.utils.sheet_to_json(worksheet);
-
-          const fileName = path.basename(filePath, ".xlsx");
-          const jsonFilePath = `cypress/fixtures/${
-            fileName + (isOriginalFile ? "Original" : "")
-          }.json`;
-          writeFileSync(jsonFilePath, JSON.stringify(jsonData));
-          return path.basename(jsonFilePath);
+        convertXlsxToJson(args: [string, boolean]) {
+          return new Promise((resolve, reject) => {
+            const [filePath, isOriginalFile] = args;
+            const workbook = XLSX.readFile(filePath); // read the file
+            const worksheet = workbook.Sheets[workbook.SheetNames[0]]; // get the first worksheet
+            const jsonData = XLSX.utils.sheet_to_json(worksheet); // convert the worksheet to JSON
+            const fileName = path.basename(filePath, ".xlsx"); // get the file name without the extension
+            const jsonFilePath = `cypress/fixtures/${
+              fileName + (isOriginalFile ? "Original" : "")
+            }.json`; // create the json file path
+            writeFileSync(jsonFilePath, JSON.stringify(jsonData)); // write the json data to the file
+            resolve(jsonFilePath);
+          });
+        },
+        deleteFile(filePath: string) {
+          return new Promise((resolve, reject) => {
+            unlink(filePath, (err) => {
+              if (err) {
+                console.error(err);
+                return reject(err);
+              }
+              resolve(null);
+            });
+          });
         },
       });
       allureWriter(on, config);
