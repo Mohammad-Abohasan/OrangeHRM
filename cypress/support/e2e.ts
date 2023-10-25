a02c75db0f210ce66b4ece51d32adddbb4fdd707
Mohammad Abohasan <mo7ammad.saed@gmail.com>
2023-10-25 23:16:58 +0300

    install `cypress-mochawesome-reporter`

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -16,6 +16,7 @@
 // Import commands.js using ES2015 syntax:
 import "./commands";
 import "@shelex/cypress-allure-plugin";
+import "cypress-mochawesome-reporter/register";
 import "cypress-plugin-api";
 import "../support/utils/apiUtils";
 
