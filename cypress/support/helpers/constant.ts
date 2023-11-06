3ef79688a25d73612f395ba5b8b8b1382f4d3581
Mohammad Abohasan <mo7ammad.saed@gmail.com>
2023-11-06 04:17:27 +0200

    Add report fields to SystemFields in support/helpers/constants

--- cypress/support/helpers/constant.ts
+++ cypress/support/helpers/constant.ts
@@ -39,4 +39,10 @@ export const SystemFields: { [key: string]: string } = {
   "CandiDate Name": INPUT_TYPE.ListBox,
   "CandiDate Status": INPUT_TYPE.Dropdown,
   "Date of Application": INPUT_TYPE.Date,
+  "Add - Report Name": INPUT_TYPE.Text,
+  "Search - Report Name": INPUT_TYPE.ListBox,
+  "Selection Criteria": INPUT_TYPE.Dropdown,
+  "Select Display Field Group": INPUT_TYPE.Dropdown,
+  "Select Display Field": INPUT_TYPE.Dropdown,
+  Location: INPUT_TYPE.Dropdown,
 };
