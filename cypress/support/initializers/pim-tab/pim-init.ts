1c033127c314c0b945b7b7f9328cf7ca8f470ca1
Mohammad Abohasan <mo7ammad.saed@gmail.com>
2023-11-06 04:08:46 +0200

    Add an initializer for the employee job details and salary component in support/initializer/pim-tab/pim-init

--- cypress/support/initializers/pim-tab/pim-init.ts
+++ cypress/support/initializers/pim-tab/pim-init.ts
@@ -1,4 +1,6 @@
 import { ICreateEmployeePayload } from "../../apis/payload/pim-tab/add-employee-payload";
+import { ICreateEmployeeSalaryComponentsPayload } from "../../apis/payload/pim-tab/add-employee-salary-components-payload";
+import { IUpdateEmployeeJobDetailsPayload } from "../../apis/payload/pim-tab/update-employee-job-details-payload";
 import SharedHelper from "../../helpers/shared-helper";
 
 export default class PimInit {
@@ -10,6 +12,25 @@ export default class PimInit {
       middleName,
       lastName,
       empPicture,
+    };
+    return payload;
+  }
+
+  static initUpdateEmployeeJobDetails(
+    employeeJobDetails: IUpdateEmployeeJobDetailsPayload
+  ): IUpdateEmployeeJobDetailsPayload {
+    const payload = {
+      ...employeeJobDetails,
+    };
+    return payload;
+  }
+
+  static initEmployeeSalaryComponentsDetails(
+    employeeSalaryComponentsDetails: ICreateEmployeeSalaryComponentsPayload
+  ): ICreateEmployeeSalaryComponentsPayload {
+    const payload = {
+      ...employeeSalaryComponentsDetails,
+      salaryAmount: `${SharedHelper.generateRandomNumber(50000, 60000)}`,
     };
     return payload;
   }
