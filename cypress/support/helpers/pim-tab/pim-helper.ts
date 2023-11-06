035d45c285029790b52915f2ece263c77694d94c
Mohammad Abohasan <mo7ammad.saed@gmail.com>
2023-11-06 04:12:02 +0200

    Create an update/add for the employee job details/salary component in support/helpers/pim-tab/pim-helper

--- cypress/support/helpers/pim-tab/pim-helper.ts
+++ cypress/support/helpers/pim-tab/pim-helper.ts
@@ -1,4 +1,6 @@
 import { ICreateEmployeePayload } from "../../apis/payload/pim-tab/add-employee-payload";
+import { ICreateEmployeeSalaryComponentsPayload } from "../../apis/payload/pim-tab/add-employee-salary-components-payload";
+import { IUpdateEmployeeJobDetailsPayload } from "../../apis/payload/pim-tab/update-employee-job-details-payload";
 import PimInit from "../../initializers/pim-tab/pim-init";
 import SharedInit from "../../initializers/shared-init";
 
@@ -23,5 +25,31 @@ export default class PimHelper {
       URLs.employees,
       SharedInit.initDeleteItem(empNumber)
     );
+  }
+
+  static updateEmployeeJobDetails(
+    employeeJobDetails: IUpdateEmployeeJobDetailsPayload,
+    empNumber: number
+  ) {
+    return cy
+      .updateEmployeeJobDetails(
+        "PUT",
+        `${URLs.employees}/${empNumber}/job-details`,
+        PimInit.initUpdateEmployeeJobDetails(employeeJobDetails)
+      )
+      .then((response) => response.data);
+  }
+
+  static addEmployeeSalaryComponents(
+    employeeSalaryComponentsDetails: ICreateEmployeeSalaryComponentsPayload,
+    empNumber: number
+  ) {
+    return cy
+      .addEmployeeSalaryComponents(
+        "POST",
+        `${URLs.employees}/${empNumber}/salary-components`,
+        PimInit.initEmployeeSalaryComponentsDetails(employeeSalaryComponentsDetails)
+      )
+      .then((response) => response.data);
   }
 }
