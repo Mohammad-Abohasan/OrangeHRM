56b16d602753a15ba43ed443a4a5d5e3d3fd38cd
Mohammad Abohasan <mo7ammad.saed@gmail.com>
2023-11-08 23:46:27 +0200

    Add interfaces for payload & response `add claim` `request` in support/apis

---
+++ cypress/support/apis/response/claim-tab/add-claim-request-response.ts
@@ -0,0 +1,26 @@
+export interface ICreateClaimRequestResponse {
+  data: Data;
+  meta: any[];
+  rels: any[];
+}
+
+export interface Data {
+  id: number;
+  referenceId: string;
+  claimEvent: ClaimEvent;
+  currencyType: CurrencyType;
+  remarks: string;
+  status: string;
+}
+
+export interface ClaimEvent {
+  id: number;
+  name: string;
+  status: boolean;
+  isDeleted: boolean;
+}
+
+export interface CurrencyType {
+  id: string;
+  name: string;
+}
