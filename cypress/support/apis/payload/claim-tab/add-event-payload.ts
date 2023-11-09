0ebadc50dfc4cf6f5cc98852911c4f1027a1ee4e
Mohammad Abohasan <mo7ammad.saed@gmail.com>
2023-11-09 02:55:46 +0200

    Fix/ swap interfaces content in add-event payload and response

--- cypress/support/apis/payload/claim-tab/add-event-payload.ts
+++ cypress/support/apis/payload/claim-tab/add-event-payload.ts
@@ -1,11 +1,4 @@
 export interface ICreateEventPayload {
-  data: Data;
-  meta: any[];
-  rels: any[];
-}
-
-export interface Data {
-  id: number;
   name: string;
   description: string;
   status: boolean;
--- cypress/support/apis/response/claim-tab/add-event-response.ts
+++ cypress/support/apis/response/claim-tab/add-event-response.ts
@@ -1,4 +1,11 @@
 export interface ICreateEventResponse {
+  data: Data;
+  meta: any[];
+  rels: any[];
+}
+
+export interface Data {
+  id: number;
   name: string;
   description: string;
   status: boolean;
