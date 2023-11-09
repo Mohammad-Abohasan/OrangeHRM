45d93f8605073f67da0ff189093396436a0f3775
Mohammad Abohasan <mo7ammad.saed@gmail.com>
2023-11-10 00:55:13 +0200

    Refactor code for improved readability and maintainability in Claim tab test suite

--- cypress/e2e/claim-tab/claim-tab.spec.cy.ts
+++ cypress/e2e/claim-tab/claim-tab.spec.cy.ts
@@ -7,151 +7,118 @@ import SharedHelper from "../../support/helpers/shared-helper";
 
 const claimTabActions: ClaimTabActions = new ClaimTabActions();
 const claimTabAssertions: ClaimTabAssertions = new ClaimTabAssertions();
+
+let employeeData: any = {};
+let empLoginData: any = {};
+let claimReqData: any = {};
+let eventData: any = {};
+
 describe("Claim: Claim Tab functionality", () => {
   beforeEach(() => {
     cy.loginOrangeHRM();
-
-    cy.fixture("pim-tab/employeeInfo.json").as("employeeData");
-    cy.fixture("admin-tab/user-management-page/adminInfo.json").as("empLoginData");
-    cy.fixture("claim-tab/claimRequestInfo.json").as("claimReqData");
-    cy.fixture("claim-tab/eventInfo.json").as("eventData");
-
-    cy.get("@employeeData")
+    cy.fixture("pim-tab/employeeInfo.json")
+      .then((employeeInfo: any) => {
+        employeeData = employeeInfo;
+        cy.fixture("admin-tab/user-management-page/adminInfo.json");
+      })
+      .then((adminInfo: any) => {
+        empLoginData = adminInfo;
+        cy.fixture("claim-tab/claimRequestInfo.json");
+      })
+      .then((claimRequestInfo: any) => {
+        claimReqData = claimRequestInfo;
+        cy.fixture("claim-tab/eventInfo.json");
+      })
       // Add an employee
-      .then((employeeData: any) => {
-        PimHelper.addEmployee(employeeData).then((empResponse: any) => {
-          employeeData.firstName = empResponse.firstName;
-          cy.wrap(employeeData)
-            .as("employeeData")
-            .then(() => {
-              return empResponse;
-            });
-        });
+      .then((eventInfo: any) => {
+        eventData = eventInfo;
+        PimHelper.addEmployee(employeeData);
       })
       // Add employee's login credentials
       .then((empResponse: any) => {
-        cy.wrap(empResponse.empNumber).as("empNumber");
-        cy.get("@empLoginData").then((empLoginData: any) => {
-          AdminHelper.addAdmin(empLoginData, empResponse.empNumber).then(
-            (empLoginResponse: any) => {
-              empLoginData.username = empLoginResponse.userName;
-              cy.wrap(empLoginData).as("empLoginData");
-            }
-          );
-        });
+        employeeData.firstName = empResponse.firstName;
+        employeeData.empNumber = empResponse.empNumber;
+        AdminHelper.addAdmin(empLoginData, empResponse.empNumber);
       })
       // Add an event
-      .then(() => {
-        cy.get("@eventData").then((eventData: any) => {
-          ClaimHelper.addEvent(eventData).then((eventResponse: any) => {
-            eventData.eventName = eventResponse.name;
-            cy.wrap(eventResponse.id).as("eventId");
-            cy.wrap(eventData)
-              .as("eventData")
-              .then(() => {
-                return eventResponse;
-              });
-          });
-        });
+      .then((empLoginResponse: any) => {
+        empLoginData.username = empLoginResponse.userName;
+        ClaimHelper.addEvent(eventData);
       })
       // Login as employee and add a claim request
       .then((eventResponse: any) => {
+        eventData.id = eventResponse.id;
+        eventData.eventName = eventResponse.name;
         cy.logoutOrangeHRM();
-        cy.get("@empLoginData").then((empLoginData: any) => {
-          cy.loginOrangeHRM(empLoginData.username, empLoginData.password);
-          cy.get("@claimReqData").then((claimReqData: any) => {
-            return ClaimHelper.addClaimRequest(claimReqData, eventResponse.id);
-          });
-        });
+        cy.loginOrangeHRM(empLoginData.username, empLoginData.password);
+        ClaimHelper.addClaimRequest(claimReqData, eventResponse.id);
       })
-      // Submit the claim request
-      .then((claimReqResponse: any) => {
-        cy.wrap(claimReqResponse).as("claimRequest");
-        ClaimHelper.submitClaimRequest(claimReqResponse.id);
+      // Submit the claim request and store claim request details to verify later
+      .then((claimReqRes: any) => {
+        claimReqData.employeeName = (({ firstName, middleName, lastName }) => ({
+          firstName,
+          middleName,
+          lastName,
+        }))(employeeData);
+        claimReqData.referenceId = claimReqRes.referenceId;
+        claimReqData.eventName = claimReqRes.claimEvent.name;
+        claimReqData.description = claimReqRes.remarks;
+        claimReqData.currency = claimReqRes.currencyType.name;
+        claimReqData.submittedDate = claimReqRes.referenceId.replace(
+          /^(\d{4})(\d{2})(\d{2}).*/,
+          "$1-$2-$3"
+        );
+        claimReqData.amount = "0.00";
+        ClaimHelper.submitClaimRequest(claimReqRes.id);
       })
       // login as admin
-      .then(() => {
+      .then((submitClaimRes: any) => {
+        claimReqData.status = `${submitClaimRes.status[0]}${submitClaimRes.status
+          .slice(1)
+          .toLowerCase()}`;
         cy.logoutOrangeHRM();
         cy.loginOrangeHRM();
-        // Prepare the claim request data we need to assert
-        cy.get("@employeeData").then((employeeData: any) => {
-          cy.get("@claimRequest").then((claimRequest: any) => {
-            const employeeClaimReq = {
-              employeeName: {
-                firstName: employeeData.firstName,
-                middleName: employeeData.middleName,
-                lastName: employeeData.lastName,
-              },
-              referenceId: claimRequest.referenceId,
-              eventName: claimRequest.claimEvent.name,
-              description: claimRequest.remarks,
-              currency: claimRequest.currencyType.name,
-              submittedDate: claimRequest.referenceId.replace(
-                /^(\d{4})(\d{2})(\d{2}).*/,
-                "$1-$2-$3"
-              ),
-              status: "Submitted",
-              amount: "0.00",
-            };
-            cy.wrap(employeeClaimReq).as("employeeClaimReq");
-          });
-        });
       });
+    claimTabActions.openClaimTab();
   });
 
   it("Claim - The admin should be able to approve the employee's claim request, and the employee should be able to view the status of the claim request", () => {
-    cy.get("@employeeClaimReq").then((employeeClaimReq: any) => {
-      claimTabActions.openClaimTab();
-      claimTabActions.openEmployeeClaimsPage();
-      claimTabActions.searchClaimRequest(employeeClaimReq);
-      SharedHelper.checkLoadingSpinnerIsExist(false);
-      claimTabActions.viewDetailsOfClaimRequest();
-      claimTabActions.approveClaimRequest();
-      employeeClaimReq.status = "Paid";
+    claimTabActions.openEmployeeClaimsPage();
+    claimTabActions.searchClaimRequest(claimReqData);
+    SharedHelper.checkLoadingSpinnerIsExist(false);
+    claimTabActions.viewDetailsOfClaimRequest();
+    claimTabActions.approveClaimRequest();
+    claimReqData.status = "Paid";
+    cy.logoutOrangeHRM();
 
-      cy.logoutOrangeHRM();
-      // Login as employee and verify the status of the claim request
-      cy.get("@empLoginData").then((empLoginData: any) => {
-        cy.loginOrangeHRM(empLoginData.username, empLoginData.password);
-        claimTabActions.openClaimTab();
-        claimTabActions.searchClaimRequest(employeeClaimReq);
-        claimTabAssertions.verifyClaimRequestDetails(employeeClaimReq);
-      });
-    });
+    // Login as employee and verify the status of the claim request
+    cy.loginOrangeHRM(empLoginData.username, empLoginData.password);
+    claimTabActions.openClaimTab();
+    claimTabActions.searchClaimRequest(claimReqData);
+    claimTabAssertions.verifyClaimRequestDetails(claimReqData);
   });
 
   it("Claim - The admin should be able to reject the employee's claim request, and the employee should be able to view the status of the claim request", () => {
-    cy.get("@employeeClaimReq").then((employeeClaimReq: any) => {
-      claimTabActions.openClaimTab();
-      claimTabActions.openEmployeeClaimsPage();
-      claimTabActions.searchClaimRequest(employeeClaimReq);
-      SharedHelper.checkLoadingSpinnerIsExist(false);
-      claimTabActions.viewDetailsOfClaimRequest();
-      claimTabActions.rejectClaimRequest();
-      employeeClaimReq.status = "Rejected";
+    claimTabActions.openEmployeeClaimsPage();
+    claimTabActions.searchClaimRequest(claimReqData);
+    SharedHelper.checkLoadingSpinnerIsExist(false);
+    claimTabActions.viewDetailsOfClaimRequest();
+    claimTabActions.rejectClaimRequest();
+    claimReqData.status = "Rejected";
+    cy.logoutOrangeHRM();
 
-      cy.logoutOrangeHRM();
-      // Login as employee and verify the status of the claim request
-      cy.get("@empLoginData").then((empLoginData: any) => {
-        cy.loginOrangeHRM(empLoginData.username, empLoginData.password);
-        claimTabActions.openClaimTab();
-        claimTabActions.searchClaimRequest(employeeClaimReq);
-        claimTabAssertions.verifyClaimRequestDetails(employeeClaimReq);
-      });
-    });
+    // Login as employee and verify the status of the claim request
+    cy.loginOrangeHRM(empLoginData.username, empLoginData.password);
+    claimTabActions.openClaimTab();
+    claimTabActions.searchClaimRequest(claimReqData);
+    claimTabAssertions.verifyClaimRequestDetails(claimReqData);
   });
 
   afterEach(() => {
     cy.logoutOrangeHRM();
     // Login as admin
     cy.loginOrangeHRM();
-    // Delete the employee
-    cy.get("@empNumber").then((empNumber: any) => {
-      PimHelper.deleteEmployee(empNumber);
-    });
-    // Delete the event
-    cy.get("@eventId").then((eventId: any) => {
-      ClaimHelper.deleteEvent(eventId);
-    });
+    PimHelper.deleteEmployee(employeeData.empNumber);
+    ClaimHelper.deleteEvent(eventData.id);
   });
 });
