c9bb860c79cb5422a4a67ed56e04f4aebccfb6ec
Mohammad Abohasan <mo7ammad.saed@gmail.com>
2023-11-10 09:51:06 +0200

    Refactor Cypress config file.

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,4 @@
-import { writeFileSync, unlink } from "fs";
 import { defineConfig } from "cypress";
-import { configureAllureAdapterPlugins } from "@mmisty/cypress-allure-adapter/plugins";
-// import allureWriter from "@shelex/cypress-allure-plugin/writer";
-import * as XLSX from "xlsx";
-import * as path from "path";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -12,46 +7,10 @@ export default defineConfig({
   reporter: "cypress-mochawesome-reporter",
   e2e: {
     baseUrl: "https://opensource-demo.orangehrmlive.com",
-    setupNodeEvents(on, config) {
+    specPattern: ["cypress/e2e/**/*.cy.ts", "cypress/e2e/*.feature"],
+    async setupNodeEvents(on, config) {
       // implement node event listeners here
-      on("task", {
-        convertXlsxToJson(args: [string, boolean]) {
-          return new Promise((resolve, reject) => {
-            const [filePath, isOriginalFile] = args;
-            const workbook = XLSX.readFile(filePath); // read the file
-            const worksheet = workbook.Sheets[workbook.SheetNames[0]]; // get the first worksheet
-            const jsonData = XLSX.utils.sheet_to_json(worksheet); // convert the worksheet to JSON
-            const fileName = path.basename(filePath, ".xlsx"); // get the file name without the extension
-            const jsonFilePath = `cypress/fixtures/${
-              fileName + (isOriginalFile ? "Original" : "")
-            }.json`; // create the json file path
-            writeFileSync(jsonFilePath, JSON.stringify(jsonData)); // write the json data to the file
-            resolve(jsonFilePath);
-          });
-        },
-        deleteFile(filePath: string) {
-          return new Promise((resolve, reject) => {
-            unlink(filePath, (err) => {
-              if (err) {
-                console.error(err);
-                return reject(err);
-              }
-              resolve(null);
-            });
-          });
-        },
-      });
-      // allureWriter(on, config);
-      require("cypress-mochawesome-reporter/plugin")(on);
-      require("@cypress/grep/src/plugin")(config);
-      configureAllureAdapterPlugins(on, config);
-      return config;
+      return require("./cypress/plugins")(on, config);
     },
-    env: {
-      allure: true,
-      allureAttachRequests: true,
-      allureShowDuplicateWarn: true,
-    },
-    screenshotOnRunFailure: true,
   },
 });
